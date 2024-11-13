715acbca85c9944ed9fbfd1fa31989ff07ef2cb1
Sardor0047 <macbookpro@MacBook-Pro-MacBook.local>
2024-11-13 12:50:43 +0500

    Ozgarish bilan tugatilindi

--- contact.py
+++ contact.py
@@ -6,16 +6,15 @@ class Contact:
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
-    def check_phone_number(self,name=False):
-        self.name = name
-        if len(self.name) == 17 :
-            return True
-        else :
-            return False
+
+    def check_phone_number(self):
+        if len(self.phone_number) == 17 and self.phone_number[0] == '+' and self.phone_number.count(' ') == 4 and self.phone_number[4] == ' ' and self.phone_number[7] == ' ':
+                return True
+        return False
         
     def check_name(self):
         if self.first_name[0].isupper() and self.last_name[0].isupper():
             return True
         else :
             return False
-print(len('+998 93 123 45 67'))
+a = Contact('Zarif','Naxalov','+998 93 123 45 67')
